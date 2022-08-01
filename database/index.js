8e019d4a5648aac94d1f5f92e4e006ec6fd0ab1d
namikazepian <namikazepian@gmail.com>
2022-08-01 16:50:08 +0700

    6th deploy

--- database/index.js
+++ database/index.js
@@ -2,9 +2,22 @@ const mongoose = require("mongoose");
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require("../app/config");
 
 mongoose.connect(
-	"mongodb+srv://" + dbUser + ":" + dbPass + "@" + dbHost + "/" + dbName
-	// "?retryWrites=true&w=majority"
+	"mongodb+srv://devy:devy@cluster0.jtol3.mongodb.net/cloudbread?retryWrites=true&w=majority"
+	// "mongodb+srv://" +
+	// 	dbUser +
+	// 	":" +
+	// 	dbPass +
+	// 	"@" +
+	// 	dbHost +
+	// 	"/" +
+	// 	dbName +
+	// 	"?retryWrites=true&w=majority",
+	// {
+	// 	useUnifiedTopology: true,
+	// 	useNewUrlParser: true,
+	// }
 );
+// mongodb+srv://<username>:<password>@cluster0.jtol3.mongodb.net/?retryWrites=true&w=majority
 // mongoose.connect(
 // 	`mongodb%2Bsrv%3A%2F%2F${dbUser}%3A${dbPass}%40${dbHost}%2F${dbName}`
 // );
