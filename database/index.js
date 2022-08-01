905958fb064a14ed0d280811682aeda4b1214a99
namikazepian <namikazepian@gmail.com>
2022-08-01 13:05:22 +0700

    2nd deploy

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const { dbHost, dbPass, dbName, dbPort, dbUser } = require("../app/config");
 
-// mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}%40${dbHost}/${dbName}`);
-mongoose.connect(
-	`mongodb%2Bsrv%3A%2F%2F${dbUser}%3A${dbPass}%40${dbHost}%2F${dbName}`
-);
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}`);
+// mongoose.connect(
+// 	`mongodb%2Bsrv%3A%2F%2F${dbUser}%3A${dbPass}%40${dbHost}%2F${dbName}`
+// );
 
 const db = mongoose.connection;
 module.exports = db;
